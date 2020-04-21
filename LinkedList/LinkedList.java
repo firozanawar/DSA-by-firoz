550885846ccec0cbef2d3f821be57233826fef58
firoz.anawar <firoz.anawar@timesinternet.in>
2020-04-21 14:46:58 +0530

    LinkedList append,insert,delete,count,reverse done

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -14,6 +14,11 @@ public class LinkedList {
         }
     }
 
+    /**
+     * insert At Beginning of list
+     *
+     * @param data
+     */
     private void insertAtBeginning(int data) {
 
         Node new_node = new Node(data);
@@ -21,6 +26,11 @@ public class LinkedList {
         head = new_node;
     }
 
+    /**
+     * insert At End of list
+     *
+     * @param data
+     */
     private void insertAtEnd(int data) {
         Node new_node = new Node(data);
         Node temp = head;
@@ -30,6 +40,23 @@ public class LinkedList {
         }
 
         temp.next = new_node;
+    }
+
+    private void append(int new_data) {
+        Node new_node = new Node(new_data);
+        if (head == null) {
+            head = new Node(new_data);
+            return;
+        }
+
+        new_node.next = null;
+        Node tempNode = head;
+        while (tempNode.next != null) {
+            tempNode = tempNode.next;
+        }
+
+        tempNode.next = new_node;
+        return;
     }
 
     private void insertAt(Node prev_node, int new_data) {
@@ -44,13 +71,84 @@ public class LinkedList {
         prev_node.next = new_node;
     }
 
-    private static int getCount() {
-        // TODO: 21/04/20
-        return 0;
+    private int getNodeCount(Node node) {
+        if (node == null) {
+            return 0;
+        }
+
+        return 1 + getNodeCount(node.next);
+    }
+
+    private int getCount(Node node) {
+        return getNodeCount(node);
     }
 
     private void deleteNode(int key) {
+        Node temp = head, prev = null;
 
+        // # case 1 - delete the head
+        if (temp != null && temp.data == key) {
+            head = temp.next;
+            return;
+        }
+
+        // # case 2 - search node and delete
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        // # case 3 -  when item is not present.
+        if (temp == null) {
+            return;
+        }
+
+        prev.next = temp.next; // Linking the previous to next node by delinking
+    }
+
+    private void deleteLinkedlList() {
+        head = null;
+    }
+
+    private void reversByIterative(Node curr) {
+        if (curr == null || curr.next == null)
+            return;
+
+        Node prev = null;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        printListUsingHead(prev);
+    }
+
+    private Node reversByRecursive(Node curr) {
+        Node node = reverse(curr);
+        return node;
+    }
+
+    private Node reverse(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node newHeadNode = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return newHeadNode;
+    }
+
+    private void printListUsingHead(Node head) {
+
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
     }
 
     private void printList() {
@@ -71,7 +169,17 @@ public class LinkedList {
 
         linkedList.insertAtEnd(30);
         linkedList.insertAtEnd(40);
+        linkedList.append(50);
 
         linkedList.printList();
+
+        //System.out.println();
+        //linkedList.reversByIterative(linkedList.head);
+
+        System.out.println();
+        Node newHead = linkedList.reversByRecursive(linkedList.head);
+        linkedList.printListUsingHead(newHead);
+        System.out.println();
+        System.out.println("Linkedlist count is: " + linkedList.getCount(newHead));
     }
 }
